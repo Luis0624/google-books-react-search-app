23503f0ef8a53a9a346e0392f812182a7381cbfd
Luis0624 <luis.apgo12@gmail.com>
2021-07-05 21:15:08 -0400

    heroku deployement

--- book-search-app/server.js
+++ book-search-app/server.js
@@ -25,6 +25,10 @@ mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
